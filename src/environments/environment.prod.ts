8a5316d2c45f6d3123603f732d7584e541e521ad
Onyx <200004@utags.edu.mx>
2023-04-11 19:27:28 -0600

    subiendo el proyecto nuevo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'food-fast-19010',
+    appId: '1:174931707884:web:c36276c3f435419f2922c9',
+    storageBucket: 'food-fast-19010.appspot.com',
+    apiKey: 'AIzaSyD337xwzEPYivraLKsKL_6y9lSS97gAw-4',
+    authDomain: 'food-fast-19010.firebaseapp.com',
+    messagingSenderId: '174931707884',
+    measurementId: 'G-2ZJR56G4Q0',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'food-fast-19010',
+    appId: '1:174931707884:web:c36276c3f435419f2922c9',
+    storageBucket: 'food-fast-19010.appspot.com',
+    apiKey: 'AIzaSyD337xwzEPYivraLKsKL_6y9lSS97gAw-4',
+    authDomain: 'food-fast-19010.firebaseapp.com',
+    messagingSenderId: '174931707884',
+    measurementId: 'G-2ZJR56G4Q0',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
